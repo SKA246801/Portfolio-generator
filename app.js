9fe13c2be12b7c3e0011ed9eb0ba509fc7cf7ac5
Sebastian <sebastianalvarado246801@gmail.com>
2021-05-12 18:41:46 -0500

    switching branches

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length)
+inquirer.prompt([
+    {
+        type: 'imput',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(answers => console.log(answers))
+// const fs = require('fs')
+// const generatePage = require('./src/page-template.js')
 
-const name = profileDataArgs[0]
-const github = profileDataArgs[1]
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err
   
-  console.log('Complete ')
-})
-
+//   console.log('Complete ')
+// })
